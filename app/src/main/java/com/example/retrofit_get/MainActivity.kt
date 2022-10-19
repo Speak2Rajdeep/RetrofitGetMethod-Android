4558de1712a26420fa91798d1430a3cb45abcc77
rajdeeps <rajdeepsarkar271@gmail.com>
2022-10-19 11:52:12 +0530

    Added Progress Bar and Implemented CardView

--- app/src/main/java/com/example/retrofit_get/MainActivity.kt
+++ app/src/main/java/com/example/retrofit_get/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.retrofit_get
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,13 +18,29 @@ import retrofit2.converter.gson.GsonConverterFactory
 const val baseURL = "https://jsonplaceholder.typicode.com/"
 
 class MainActivity : AppCompatActivity() {
+
+    private var responseBody: ArrayList<MyDataItem> = ArrayList()
+    lateinit var myAdapter: MyAdapter
+    lateinit var linearLayoutManager: LinearLayoutManager
+    lateinit var progressBar : ProgressBar
+    lateinit var loading : TextView
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar = findViewById(R.id.progress)
+        loading = findViewById(R.id.loading)
+        recyclerView = findViewById(R.id.recycler)
+
+        recycler.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(this)
+        recycler.layoutManager = linearLayoutManager
+
+        myAdapter = MyAdapter(baseContext, responseBody)
         getMyData()
     }
 
-    private fun getMyData(){
+    private fun getMyData() {
         val retrofitBuilder = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(baseURL)
@@ -31,22 +52,20 @@ class MainActivity : AppCompatActivity() {
         retrofitData.enqueue(object : Callback<List<MyDataItem>> {
             override fun onResponse(
                 call: Call<List<MyDataItem>>,
-                response: Response<List<MyDataItem>>) {
-                val responseBody = response.body()!!
-                val strBuilder = StringBuilder()
+                response: Response<List<MyDataItem>>
+            ) {
+                response.body()?.let { responseBody.addAll(it) }
 
-                for(data in responseBody){
-                    strBuilder.append(data.id)
-                    strBuilder.append("  ")
-                    strBuilder.append(data.title)
-                    strBuilder.append("\n")
-                }
-                val textView = findViewById<TextView>(R.id.textview)
-                textView.text = strBuilder
+                myAdapter.notifyDataSetChanged()
+                recycler.adapter = myAdapter
+                progressBar.visibility = View.GONE
+                loading.visibility = View.GONE
+                recyclerView.visibility  = View.VISIBLE
+
             }
 
             override fun onFailure(call: Call<List<MyDataItem>>, t: Throwable) {
-                Toast.makeText(this@MainActivity,"Wrong : $(t.message)",Toast.LENGTH_LONG).show()
+                Toast.makeText(this@MainActivity, "Wrong : ${t.message}", Toast.LENGTH_LONG).show()
             }
         })
 
---
+++ app/src/main/java/com/example/retrofit_get/MyAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.retrofit_get
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.row_items.view.*
+
+class MyAdapter(val context : Context,val userList: List<MyDataItem> ) : RecyclerView.Adapter<MyAdapter.ViewHolder>(){
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        var id : TextView
+        var title : TextView
+
+
+        init {
+            id = itemView.id_tv
+            title = itemView.title_tv
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var itemView = LayoutInflater.from(context).inflate(R.layout.row_items,parent,false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.id.text = userList[position].id.toString()
+        holder.title.text = userList[position].title.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+}
